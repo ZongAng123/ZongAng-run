e5b369d0082ebe9b5d224f84fe9078b4a4ab0b86
ZongAng123 <zongang15@yahoo.com>
2017-02-18 13:53:30 +0800

    Initial Commit

---
+++ 纵昂快跑/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  纵昂快跑
+//
+//  Created by 纵昂 on 2017/2/18.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 纵昂快跑/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  纵昂快跑
+//
+//  Created by 纵昂 on 2017/2/18.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
